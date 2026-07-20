5301facbf916b1b82e0e97cc380d782a365d6f4f
Anoir Baya <anbaya@c3r9p10.1337.ma>
2026-07-20 11:50:15 +0100

    improved the default index: dashboard to test GET, POST, DELETE, CGI, COOKIES & SESSIONS

--- www/cgi-bin/status.py
+++ www/cgi-bin/status.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
-print("Status: 404 Not Found")
-print("Content-Type: text/plain\n")
-print("missing")
+import cgi
+
+print("Content-Type: text/html\n")
+print("<html><body>")
+print("<h1>CGI Status</h1>")
+print("<p>Script executed successfully.</p>")
+print("</body></html>")
